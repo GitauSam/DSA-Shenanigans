4ca2973e3ed3bf66c5782a8afdcfc8585bff5e2f
GitauSam <njorogegitausamuel@gmail.com>
2020-11-24 16:37:58 +0300

    Uploads custom linked list implementation

--- src/com/zenza/dsa/Main.java
+++ src/com/zenza/dsa/Main.java
@@ -1,18 +1,19 @@
 package com.zenza.dsa;
 
 import com.zenza.dsa.arrays.Array;
+import com.zenza.dsa.linkedlists.LinkedList;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(10);
-        list.add(20);
-        list.add(30);
-        list.remove(0);
-        list.indexOf(20);
-        System.out.println(list);
+        var list = new LinkedList();
+        list.addFirst(10);
+        list.addFirst(20);
+        list.addFirst(30);
+        System.out.println(list.indexOf(20));
+        System.out.println(list.contains(300));
     }
 }
--- src/com/zenza/dsa/linkedlists/LinkedList.java
+++ src/com/zenza/dsa/linkedlists/LinkedList.java
@@ -1,2 +1,107 @@
-package com.zenza.dsa.linkedlists;public class LinkedList {
+package com.zenza.dsa.linkedlists;
+
+import java.util.NoSuchElementException;
+
+public class LinkedList {
+
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node first;
+    private Node last;
+
+    // addFirst
+    public void addFirst(int val) {
+        var node = new Node(val);
+
+        if (isEmpty())
+            first = last = node;
+        else {
+            node.next = first;
+            first = node;
+        }
+    }
+
+    // addLast
+    public void addLast(int item) {
+
+        var node = new Node(item);
+
+        if (isEmpty())
+            first = last = node;
+        else {
+            last.next = node;
+            last = node;
+        }
+
+    }
+
+    // contains
+    public boolean contains(int item) {
+        return indexOf(item) != -1;
+    }
+
+    // indexOf
+    public int indexOf(int item) {
+        int index = 0;
+        var current = first;
+
+        while (current != null) {
+            if (current.value == item) return index;
+            current = current.next;
+            index++;
+        }
+
+        return -1;
+    }
+
+    // deleteFirst
+    public void removeFirst() {
+
+        if(isEmpty()) throw new NoSuchElementException();
+
+        if (first == last) {
+            first = last = null;
+        }
+        var second = first.next;
+        first.next = null;
+        first = second;
+    }
+
+    // deleteLast
+    public void removeLast() {
+        // [10 -> 20 -> 30]
+        // previous -> 20
+        // last -> 20
+        if (isEmpty()) throw new NoSuchElementException();
+
+        if (first == last) {
+            first = last = null;
+            return;
+        }
+
+        var previous = getPrevious(last);
+        last = previous;
+        last.next = null;
+    }
+
+    private Node getPrevious(Node node) {
+        var current = first;
+        while (current != null) {
+            if (current.next == last) return current;
+            current = current.next;
+        }
+
+        return null;
+    }
+
+    private boolean isEmpty() {
+        return first == null;
+    }
 }
